a4446242c6b570b93c9794f1e8aa3805e51566af
Gary <gary-christensen@uiowa.edu>
2016-09-26 17:54:33 +0000

    removed unwanted routes and removed unnecessary code from UserController

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  #get 'users/new'
 
-  get 'users/create'
+  #get 'users/create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
